84e3280770fd569d45134f9276eaddcd76d28d2a
Amen Allah Hajji <amenallah.hajji@etudiant-fst.utm.tn>
2025-11-12 22:47:14 +0100

    Ajout projet Angular complet : Bonjour, Articles, Produits, Tâches

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Bonjour} from './bonjour/bonjour';
+import {Articles} from './articles/articles';
+import {Produits} from './produits/produits';
+import {Taches} from './taches/taches';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Bonjour,Articles,Produits,Taches],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
